67e2083915ca6b3675251d3dedaa0d69ee1fee29
Nikita Semchenko <nikita.semchenko@effective-soft.com>
2022-04-26 14:44:28 +0400

    refactoring

--- src/Helpers/DateHelper.ts
+++ src/Helpers/DateHelper.ts
@@ -1,14 +1,10 @@
-﻿export abstract class DateHelper
+﻿import moment from 'moment';
+
+export abstract class DateHelper
 {
     static FormatDate(dateTime : string) : string
     {
         let date = new Date(dateTime);
-        let month = ("0" + (date.getMonth() + 1)).slice(-2);
-        let day  = ("0" + (date.getDate())).slice(-2);
-        let year = date.getFullYear();
-        let hour =  ("0" + (date.getHours())).slice(-2);
-        let min =  ("0" + (date.getMinutes())).slice(-2);
-
-        return year + "-" + month + "-" + day + " " + hour + ":" +  min;
+        return (moment(date)).format('YYYY-MMM-DD HH:mm');
     }
 }
