28921f08f48ffe3e1469b1dbe3946dce9b876d5f
Nikita Semchenko <nikita.semchenko@effective-soft.com>
2022-07-02 19:56:13 +0300

    app renaming

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import { store } from './store';
 import { Provider } from 'react-redux';
 
-//const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
